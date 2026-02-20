c1124aae78b5e5751ed7cfdd396b997e6a9f8c05
liebeSonne <>
2026-02-20 22:06:46 +0300

    iter1 - добавил проверку валидности url

--- internal/handler/validate.go
+++ internal/handler/validate.go
@@ -1,9 +1,13 @@
 package handler
 
-import "errors"
+import (
+	"errors"
+	"net/url"
+)
 
 var ErrEmptyID = errors.New("empty ID")
 var ErrEmptyURL = errors.New("empty URL")
+var ErrInvalidURL = errors.New("invalid URL")
 
 func validateShortLinkID(shortLinkID string) error {
 	if len(shortLinkID) == 0 {
@@ -12,9 +16,17 @@ func validateShortLinkID(shortLinkID string) error {
 	return nil
 }
 
-func validateLink(url string) error {
-	if len(url) == 0 {
+func validateLink(str string) error {
+	if len(str) == 0 {
 		return ErrEmptyURL
+	}
+
+	u, err := url.ParseRequestURI(str)
+	if err != nil {
+		return err
+	}
+	if u.Scheme == "" || u.Host == "" {
+		return ErrInvalidURL
 	}
 	return nil
 }
