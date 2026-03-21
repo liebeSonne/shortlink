132e7d6c78f44a2c172859a89061b147c3a7699d
liebeSonne <>
2026-03-21 21:02:44 +0300

    iter8 - добавил тесты запросов со сжатием

---
+++ internal/handler/mock_test.go
@@ -0,0 +1,61 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/liebeSonne/shortlink/internal/model"
+	"github.com/stretchr/testify/mock"
+)
+
+type mockShortLinkHandler struct {
+	mock.Mock
+}
+
+func (m *mockShortLinkHandler) HandleGet(w http.ResponseWriter, r *http.Request) {
+	m.Called(w, r)
+}
+func (m *mockShortLinkHandler) HandleCreate(w http.ResponseWriter, r *http.Request) {
+	m.Called(w, r)
+}
+
+func (m *mockShortLinkHandler) HandleCreateShorten(w http.ResponseWriter, r *http.Request) {
+	m.Called(w, r)
+}
+
+type mockService struct {
+	mock.Mock
+}
+
+func (m *mockService) Create(url string) (model.ShortLink, error) {
+	args := m.Called(url)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(model.ShortLink), args.Error(1)
+}
+
+type mockProvider struct {
+	mock.Mock
+}
+
+func (m *mockProvider) Get(id string) (model.ShortLink, error) {
+	args := m.Called(id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(model.ShortLink), args.Error(1)
+}
+
+type mockShortLink struct {
+	mock.Mock
+}
+
+func (m *mockShortLink) ID() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+func (m *mockShortLink) URL() string {
+	args := m.Called()
+	return args.String(0)
+}
