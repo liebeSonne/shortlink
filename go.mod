f9b5fcb2b5e5d9359b8a930e8adba118007d689e
liebeSonne <>
2026-04-01 23:59:54 +0300

    iter10 - добавил реализацию подключения к базе данных

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/liebeSonne/shortlink
 
-go 1.25
+go 1.25.0
 
 require (
 	github.com/stretchr/testify v1.11.1
@@ -9,10 +9,14 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-chi/chi/v5 v5.2.5 // indirect
 	github.com/go-resty/resty/v2 v2.17.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.1 // indirect
 	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/repository/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/jackc/pgx/v5"
+)
+
+type Database interface {
+	Ping(ctx context.Context) error
+}
+
+func NewDatabase(
+	dataSourceName string,
+) Database {
+	return &database{
+		dataSourceName: dataSourceName,
+	}
+}
+
+type database struct {
+	dataSourceName string
+}
+
+func (d *database) Ping(ctx context.Context) error {
+	db, err := sql.Open("pgx", d.dataSourceName)
+	if err != nil {
+		return fmt.Errorf("opening database connection: %w", err)
+	}
+
+	defer func() {
+		err = db.Close()
+		if err != nil {
+			fmt.Printf("error closing database connection: %v", err)
+		}
+	}()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return fmt.Errorf("pinging database: %w", err)
+	}
+
+	return nil
+}
