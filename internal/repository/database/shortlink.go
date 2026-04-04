8813071d68f543c8113b320553ba6778d99c9bdb
liebeSonne <>
2026-04-04 15:22:36 +0300

    iter11 - реализовал методы db репозитория

--- internal/repository/database/shortlink.go
+++ internal/repository/database/shortlink.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"database/sql"
+	"fmt"
 
 	"github.com/liebeSonne/shortlink/internal/model"
 	"github.com/liebeSonne/shortlink/internal/repository"
@@ -21,11 +22,29 @@ type shortLinkRepository struct {
 }
 
 func (r *shortLinkRepository) Find(ctx context.Context, shortID string) (*model.ShortLink, error) {
-	//TODO implement me
-	panic("implement me")
+	const sqlQuery = `
+		SELECT short_id, url FROM short_link WHERE short_id = ?
+	`
+
+	var shortLink model.ShortLink
+	row := r.db.QueryRowContext(ctx, sqlQuery, shortID)
+	err := row.Scan(shortLink.ID, &shortLink.URL)
+	if err != nil {
+		return nil, fmt.Errorf("error on scan row: %w", err)
+	}
+
+	return &shortLink, nil
 }
 
 func (r *shortLinkRepository) Store(ctx context.Context, shortLink model.ShortLink) error {
-	//TODO implement me
-	panic("implement me")
+	const sqlQuery = `
+		INSERT INTO short_link (short_id, url) VALUES (?, ?)
+	`
+
+	_, err := r.db.ExecContext(ctx, sqlQuery, shortLink.ID, shortLink.URL)
+	if err != nil {
+		return fmt.Errorf("error on insert: %w", err)
+	}
+
+	return nil
 }
