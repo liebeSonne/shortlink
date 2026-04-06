9f453d398787314891ffb496d288de389614960e
liebeSonne <>
2026-04-06 20:42:01 +0300

    iter13 - добавил map для memory репозитория для поиска по ссылке

--- internal/repository/memory/memory.go
+++ internal/repository/memory/memory.go
@@ -10,13 +10,15 @@ import (
 
 func NewMemoryShortLinkRepository() repository.ShortLinkRepository {
 	return &memoryShortLinkRepository{
-		linksMap: make(map[string]model.ShortLink),
+		linksMap:   make(map[string]model.ShortLink),
+		urlToIDMap: make(map[string]string),
 	}
 }
 
 type memoryShortLinkRepository struct {
-	linksMap map[string]model.ShortLink
-	mu       sync.RWMutex
+	linksMap   map[string]model.ShortLink
+	urlToIDMap map[string]string
+	mu         sync.RWMutex
 }
 
 func (s *memoryShortLinkRepository) Find(_ context.Context, shortID string) (*model.ShortLink, error) {
@@ -32,10 +34,13 @@ func (s *memoryShortLinkRepository) Find(_ context.Context, shortID string) (*mo
 func (s *memoryShortLinkRepository) FindByURL(_ context.Context, url string) (*model.ShortLink, error) {
 	s.mu.RLock()
 	defer s.mu.RUnlock()
-	for _, link := range s.linksMap {
-		if link.URL == url {
-			return &link, nil
+
+	if id, ok := s.urlToIDMap[url]; ok {
+		link, ok := s.linksMap[id]
+		if !ok {
+			return nil, nil
 		}
+		return &link, nil
 	}
 	return nil, nil
 }
@@ -44,6 +49,7 @@ func (s *memoryShortLinkRepository) Store(_ context.Context, shortLink model.Sho
 	s.mu.Lock()
 	defer s.mu.Unlock()
 	s.linksMap[shortLink.ID] = shortLink
+	s.urlToIDMap[shortLink.URL] = shortLink.ID
 	return nil
 }
 
@@ -52,6 +58,7 @@ func (s *memoryShortLinkRepository) StoreAll(_ context.Context, shortLinks []mod
 	defer s.mu.Unlock()
 	for _, shortLink := range shortLinks {
 		s.linksMap[shortLink.ID] = shortLink
+		s.urlToIDMap[shortLink.URL] = shortLink.ID
 	}
 	return nil
 }
