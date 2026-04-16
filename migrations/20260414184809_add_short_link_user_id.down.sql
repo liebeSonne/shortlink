7fc97e568a34caeda40e9348bc2d70ac0097e20b
liebeSonne <>
2026-04-16 22:40:51 +0300

    iter14 - убрал уникальность ссылок на пользователя

--- migrations/20260414184809_add_short_link_user_id.down.sql
+++ migrations/20260414184809_add_short_link_user_id.down.sql
@@ -1,11 +1,1 @@
-ALTER TABLE short_link DROP CONSTRAINT unique_user_url;
-
-DELETE FROM short_link WHERE id NOT IN (
-    SELECT MIN(id)
-    FROM short_link
-    GROUP BY url
-);
-
-ALTER TABLE short_link ADD CONSTRAINT unique_url UNIQUE (url);
-
 ALTER TABLE short_link DROP COLUMN user_id;
--- migrations/20260414184809_add_short_link_user_id.up.sql
+++ migrations/20260414184809_add_short_link_user_id.up.sql
@@ -1,5 +1,1 @@
 ALTER TABLE short_link ADD COLUMN user_id UUID NULL;
-
-ALTER TABLE short_link DROP CONSTRAINT unique_url;
-
-ALTER TABLE short_link ADD CONSTRAINT unique_user_url UNIQUE (url, user_id);
